c915ae58b69596052fe4c16bbe1330d2adcb66a9
ramiro1523 <ramiromunozrosas@gmail.com>
2025-10-06 22:14:08 -0500

    Delete App.test.js

--- App.test.js
+++
@@ -1,9 +0,0 @@
-// src/App.test.js
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app title', () => {
-  render(<App />);
-  const title = screen.getByText(/Plataforma de Denuncia Ciudadana/i);
-  expect(title).toBeInTheDocument();
-});
